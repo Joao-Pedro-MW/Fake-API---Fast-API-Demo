7bce3a635166e5c436627fc6dae0d6dd8cdbe8bd
João Pedro MW <jpedro01230@gmail.com>
2024-10-29 18:25:22 -0300

    WebServer & Endpoint GET Nome

---
+++ server.py
@@ -0,0 +1,14 @@
+from faker import Faker
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+faker = Faker()
+
+@app.get("/nome")
+def get_nomes():
+    return JSONResponse(content=['Nome',faker.name()])
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=80)
